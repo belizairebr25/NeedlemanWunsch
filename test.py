8abf109656a997c5d90218d8ad6b64a2a91d14da
Brician <belizairebrician@gmail.com>
2026-03-02 11:23:26 -0500

    Added tests to indicate where code needs refactor

--- test.py
+++ test.py
@@ -13,9 +13,16 @@ class testPairwise(unittest.TestCase):
         self.test_seq_op2 = "G-ATTACA"
         self.test_seq_op3 = "CAGC-TA"
         self.test_seq_op4 = "CA-CATA"
+        self.test_seq_op13 = "GACTGC-G"
+        self.test_seq_op31 = "-CA-GCTA"
+        self.test_seq_op24 = "CA-CATA"
+        self.test_seq_op42 = "GATTACA"
 
         self.optimal_score12 = 0
         self.optimal_score34 = 3
+        self.optimal_score13 = 0
+        self.optimal_score42 = -1
+
         #objects
         self.P1 = Pairwise(self.test_seq1, self.test_seq2)
         self.P2 = Pairwise(self.test_seq3, self.test_seq4)
@@ -31,6 +38,19 @@ class testPairwise(unittest.TestCase):
         self.assertEqual(str(self.P2), f"{self.test_seq_op3}\n{self.test_seq_op4}")
 
     ##### ~Test Edge Cases~ #####
+
+    def test_assymetric_input1(self):
+        """test for case where string 1 is longer than string 2"""
+        self.P3 = Pairwise(self.test_seq1, self.test_seq3)
+        self.assertEqual(self.P3.score(), self.optimal_score13)
+        self.assertEqual(str(self.P3), f"{self.test_seq_op13}\n{self.test_seq_op31}")
+
+    def test_assymetric_input2(self):
+        """test for case where string 2 is longer than string 1"""
+        self.P4 = Pairwise(self.test_seq4, self.test_seq2)
+        self.assertEqual(self.P4.score(), self.optimal_score42)
+        self.assertEqual(str(self.P4), f"{self.test_seq_op42}\n{self.test_seq_op24}")
+
     def test_improper_input(self):
         """Assert bad inputs(nonstring) raise value errors"""
         with self.assertRaises(ValueError): Pairwise(5, 6)
